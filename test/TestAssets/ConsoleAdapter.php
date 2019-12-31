afa45f8204ec97c3bdc69860890a3585555dbfbd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:31:02 -0600

    Rewriting as Laminas Project package

--- test/TestAssets/ConsoleAdapter.php
+++ test/TestAssets/ConsoleAdapter.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-console for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-console/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-console/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Console\TestAssets;
+namespace LaminasTest\Console\TestAssets;
 
-use Zend\Console\Adapter\AbstractAdapter;
+use Laminas\Console\Adapter\AbstractAdapter;
 
 /**
- * @group      Zend_Console
+ * @group      Laminas_Console
  */
 class ConsoleAdapter extends AbstractAdapter
 {
